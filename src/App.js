20efe69fa55c97c960e5932681fe9536c4437bc2
Ljf227 <ljfrotton@gmail.com>
2023-09-21 13:58:47 -0400

    first commit kind of

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import About from 'components/About';
+
+export default function App() {
+  return (
+    <main class="text-gray-400 bg-gray-900 body-font"> 
+    <About/>
+    </main>
+  );
+}
