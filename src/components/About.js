85c4a27fd88a3ed51ed6e8bca98eb743450b25ba
Ljf227 <ljfrotton@gmail.com>
2023-09-21 14:03:22 -0400

    gonna start the about page now

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function About(){
     return(
         <div>
-            <h1>Just Started the about page</h1> 
+            <h1 class="text-center font-serif">Just Started the about page</h1> 
         </div>
         );
 }
