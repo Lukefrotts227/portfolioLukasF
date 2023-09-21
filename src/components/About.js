e745cb5fc3da0b18aab0a6cd98e72168e6b0a1b3
Ljf227 <ljfrotton@gmail.com>
2023-09-21 18:08:16 -0400

    starting to come toeghter

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState,  useEffect } from "react";
 
+import "./more.css";
 
 export default function About(){
+    const [isVisible, setIsVisible] = useState(false); 
+
+    
+    useEffect(() => {
+        setTimeout(() => {
+          setIsVisible(true);
+        }, 10);
+      }, []);
+
     return(
-        <section>
+        <section class={`${isVisible ? "fadeInLeft" : ""}`}>
             <header class="bg-gray-800 py-4 flex items-center justify-center">
                 <div class="container mx-auto text-center">
                     <h1 style={{fontSize: '2rem'}} class=" text-gray-50 font-semibold">About Me</h1>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState,  useEffect } from "react";
+import Temp from '../logo192.png'; 
+
+import "./more.css";
+
 export default function Intro(){
+    const [isVisible, setIsVisible] = useState(false); 
+
+    
+    useEffect(() => {
+        // Use a setTimeout to delay the toggle of visibility to allow the component to render
+        setTimeout(() => {
+          setIsVisible(true);
+        }, 10);
+      }, []);
+   
 
     return(
         <header class="bg-gray-800 py-3 flex items-center jusitfy-center">
-            <div class="container mx-auto text-center"> 
-                <h1 class="text-gray-50">Introduction</h1>
-            </div>
+            <div style={{fontSize: '3rem'}} class={`container mx-auto text-center bg-gray-800 ${isVisible ? "fadeInRight" : ""}`}> 
+                <h1 class="text-gray-50">Hello, My name is Lukas</h1>
+                <h1 class="text-gray-50">Welcome to my Portfolio</h1> 
+                <div class="container mx-auto flex justify-center"> 
+                    <img
+                    src={Temp}
+                    alt="Logo"
+                    class="w-45 h-45 object-center rounded-full"
+                    /> 
+                </div>
+            </div>        
         </header>
     );
 }
