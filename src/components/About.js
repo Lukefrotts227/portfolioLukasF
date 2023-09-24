66697c2ee3488eb21be66ebc55902074dfccb7b5
Ljf227 <ljfrotton@gmail.com>
2023-09-24 15:38:18 -0400

    did some restructuring to prevent later headaches

--- src/components/About.js
+++ src/components/About.js
@@ -33,55 +33,65 @@ export default function About(){
 
 
 
-    return(
-        <section class={`flex flex-col ${isVisible ? "fadeInLeft" : ""}`}>
-            <header class="bg-gray-800 py-4 flex items-center justify-center">
-                <div class="container mx-auto text-center">
-                    <h1 style={{fontSize: '3rem'}} class=" text-gray-50 font-semibold">About Me</h1>
-                </div>
-            </header> 
-
-            <div class={`py-5 mx-auto text-center`}> 
-
-
-            <h3 class={`text-gray-50 med-font-u`}>Hello!</h3>
-
-
-                <div class={`max-w-md mx-auto flex justify-center items-center ${isFocused ? 'enlarge' : ''}`} 
-                onClick={() => setIsFocused(!isFocused)}>
-
-                    {isMobile?  (
-                        <div class="p-4 rounded-lg">
-                            <p class="text-gray-50 small-font-m">
-                            My name is Lukas and I am a sophmore Computer Science major at Montclair State University. 
-                            I love coding in my free time and am always looking to create new projects. 
-                            </p>
-                        </div> 
-                    ) : 
-                    (
-                        <div class={``}>
-                            <p class="text-gray-50 small-font-d">
-                                My name is Lukas and I am a sophmore Computer Science major at Montclair State University. 
-                                I love coding in my free time and am always looking to create new projects. 
-                            </p>
-                        </div>
-                    )
-                    }
-                
-                </div> 
-            </div> 
-
-            <div class="container mx-auto flex justify-center"> 
-                    <div> 
-                    <img
-                    src={Temp}
-                    alt="Logo"
-                    class="py-4 w-44 h-44f object-center rounded-full"
-                    /> 
-                    </div> 
+    
+  return (
+    <section class={`flex flex-col ${isVisible ? "fadeInLeft" : ""}`}>
+      {/* Header */}
+        {isMobile? (
+        <header class="bg-gray-800 py-4 flex items-center justify-center">
+            <div class="container mx-auto text-center">
+            <h1 class="text-gray-50 font-semibold title-font-m">About Me</h1>
             </div>
+        </header>
+        ) : 
+        (
+            <header class="bg-gray-800 py-3 flex items-center justify-center">
+                <div class="container mx-auto text-center"> 
+                    <h1 class="text-gray-50 font-semibold title-font-d">About Me</h1>
+                </div> 
+
+            </header>
+        )}
 
 
-        </section> 
-        );
-}
+        {/* Content */}
+
+        {isMobile? (
+            <content> 
+                <p class="text-gray-50 small-font-d">
+                    My name is Lukas and I am a sophomore Computer Science major at Montclair State University.
+                    I love coding in my free time and am always looking to create new projects.
+                    To hone my skills, I have and will continue to attend hackathons.
+                </p> 
+
+            </content> 
+        ) : 
+
+        (
+            <content> 
+                <p class="text-gray-50 small-font-d">
+                    My name is Lukas and I am a sophomore Computer Science major at Montclair State University.
+                    I love coding in my free time and am always looking to create new projects.
+                    To hone my skills, I have and will continue to attend hackathons.
+                </p>  
+            </content>
+        )
+    
+        }
+
+
+      {/* Additional Content */}
+
+    
+      <div class="container mx-auto flex justify-center">
+        <div>
+          <img
+            src={Temp}
+            alt="Logo"
+            class="py-4 w-44 h-44f object-center rounded-full"
+          />
+        </div>
+      </div>
+
+    </section>
+  );}
