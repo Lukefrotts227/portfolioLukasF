0339de5fa871e626f97a895a39aefc8b79793d35
Ljf227 <ljfrotton@gmail.com>
2023-09-24 18:23:00 -0400

    as of now navbar is in a good place considering my final design idea

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -34,18 +34,41 @@ export default function Intro(){
    
 
     return(
-        <header class="bg-gray-800 flex items-center jusitfy-center">
-            <div style={{fontSize: '3rem'}} class={`container mx-auto py-3 mt-16 text-center bg-gray-800 ${isVisible ? "fadeInRight" : ""}`}> 
-                <h1 class="text-gray-50">Hello, My name is Lukas</h1>
-                <h1 class="text-gray-50">Welcome to my Portfolio</h1> 
-                <div class="carousel  mx-auto flex justify-center"> 
-                    <img
-                    src={Temp}
-                    alt="Logo"
-                    class="w-20 h-20 object-center rounded-full"
-                    /> 
-                </div>
-            </div>        
-        </header>
+        <div>
+        {isMobile ? (
+            <header class="bg-gray-800 flex items-center jusitfy-center">
+                <div style={{fontSize: '3rem'}} class={`container mx-auto py-3 mt-16 text-center bg-gray-800 ${isVisible ? "fadeInRight" : ""}`}> 
+                    <h1 class="text-gray-50">Hello, My name is Lukas</h1>
+                    <h1 class="text-gray-50">Welcome to my Portfolio</h1> 
+                    <div class="carousel  mx-auto flex justify-center"> 
+                        <img
+                        src={Temp}
+                        alt="Logo"
+                        class="w-20 h-20 object-center rounded-full"
+                        /> 
+                    </div>
+                </div>        
+            </header>
+
+        )
+        : 
+        (
+            <header class="bg-gray-800 flex items-center jusitfy-center">
+                <div style={{fontSize: '3rem'}} class={`container mx-auto py-3 mt-16 text-center bg-gray-800 ${isVisible ? "fadeInRight" : ""}`}> 
+                    <h1 class="text-gray-50">Hello, My name is Lukas</h1>
+                    <h1 class="text-gray-50">Welcome to my Portfolio</h1> 
+                    <div class="carousel  mx-auto flex justify-center"> 
+                        <img
+                        src={Temp}
+                        alt="Logo"
+                        class="w-20 h-20 object-center rounded-full"
+                        /> 
+                    </div>
+                </div>        
+            </header>
+        )
+        }
+
+        </div>
     );
 }
