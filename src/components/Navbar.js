9c601395d2c4a27e96b3c5432b53355b6264c6a5
Ljf227 <ljfrotton@gmail.com>
2023-09-25 17:48:34 -0400

    still working on it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function Navbar(){
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); 
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-    //const [navbarVisible, setNavbarVisible] = useState(true);
+    
 
 
     
@@ -31,61 +31,64 @@ export default function Navbar(){
         <nav class={`fixed top-0 left-0 w-full ${
              'bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900'
           }`}>   
-            <div class="container text-center mx-auto"> 
+            
                     {isMobile ? (
-                        <div className={`md:hidden ${mobileMenuOpen ? 'block' : 'hidden'} py-4`}>  
+                        <div class={`container text-center mx-auto md:hidden ${mobileMenuOpen ? 'mobile-dropdown-enter' : 'mobile-dropdown-exit'} py-3`}>
 
-                            <div className="text-center py-4">                      
-                                <a href="#About" class="block text-white hover:text-gray-300 my-2">
-                                    About
-                                </a>
-                                <a href="#InTouch" class="block text-white hover:text-gray-300 my-2">
-                                    Get in Touch
-                                </a>
-                                <a href="#Projects" class="block text-white hover:text-gray-300 my-2">
-                                    Projects
-                                </a>
-                                <a href="#Skills" class="block text-white hover:text-gray-300 my-2">
-                                    Skills
-                                </a>
-                                <a href="#Academics" class="block text-white hover:text-gray-300 my-2">
-                                    Academics
-                                </a>
-                                <a href="#Next" class="block text-white hover:text-gray-300 my-2">
-                                    Whats Next?
-                                </a>
+                                <div class="text-center py-4">                      
+                                    <a href="#About" class="block text-white hover:text-gray-300 my-2">
+                                        About
+                                    </a>
+                                    <a href="#InTouch" class="block text-white hover:text-gray-300 my-2">
+                                        Get in Touch
+                                    </a>
+                                    <a href="#Projects" class="block text-white hover:text-gray-300 my-2">
+                                        Projects
+                                    </a>
+                                    <a href="#Skills" class="block text-white hover:text-gray-300 my-2">
+                                        Skills
+                                    </a>
+                                    <a href="#Academics" class="block text-white hover:text-gray-300 my-2">
+                                        Academics
+                                    </a>
+                                    <a href="#Next" class="block text-white hover:text-gray-300 my-2">
+                                        Whats Next?
+                                    </a>
 
-                                <a href="#Resume" class="block text-white hover:text-gray-300 my-2"> 
-                                    Resume
-                                </a> 
-                            </div> 
+                                    <a href="#Resume" class="block text-white hover:text-gray-300 my-2"> 
+                                        Resume
+                                    </a> 
+                                </div> 
+                            
                         </div>
                     ): (
-                    <div class="flex justify-center items-center mx-auto">
-                        <div class="md:flex space-x-4"> 
-                            <a href="#About" class="text-white hover:text-gray-300 text-xl">About</a>
-                            <a href="#InTouch" class="text-white hover:text-gray-300 text-xl">Get in Touch</a>
-                            <a href="#Projects" class="text-white hover:text-gray-300 text-xl">Projects</a>
-                            <a href="#Skills" class="text-white hover:text-gray-300 text-xl">Skills</a>
-                            <a href="#Academics" class="text-white hover:text-gray-300 text-xl">Academics</a>
-                            <a href="#Next" class ="text-white hover:text-gray-300 text-xl">Whats Next?</a>
-                            <a href="#Resume" class="text-white hover:text-gray-300 text-xl">Resume</a>
+                    <div class="container text-center mx-auto">
+                        <div class="flex justify-center items-center mx-auto">
+                            <div class="md:flex space-x-4"> 
+                                <a href="#About" class="text-white hover:text-gray-300 text-xl">About</a>
+                                <a href="#InTouch" class="text-white hover:text-gray-300 text-xl">Get in Touch</a>
+                                <a href="#Projects" class="text-white hover:text-gray-300 text-xl">Projects</a>
+                                <a href="#Skills" class="text-white hover:text-gray-300 text-xl">Skills</a>
+                                <a href="#Academics" class="text-white hover:text-gray-300 text-xl">Academics</a>
+                                <a href="#Next" class ="text-white hover:text-gray-300 text-xl">Whats Next?</a>
+                                <a href="#Resume" class="text-white hover:text-gray-300 text-xl">Resume</a>
+                            </div> 
                         </div> 
-                    </div> 
+                    </div>
 
                     )}
                 {isMobile && (
           // Mobile menu button
-          <div className="md:hidden">
+          <div class="md:hidden text-center">
             <button
               onClick={toggleMobileMenu}
-              className="text-white hover:text-gray-300"
+              class="text-white hover:text-gray-300"
             >
               {mobileMenuOpen ? 'Close' : 'Menu'}
             </button>
           </div>
         )}
-            </div>
+            
         </nav>
     );  
 }
